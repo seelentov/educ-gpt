2d240e5be5623650fbd20839e877fbfb569709ab
vladisluw <komkov222111@gmail.com>
2025-03-14 18:27:55 +0500

    Update verify_answer_req.go

--- app/http/dtos/verify_answer_req.go
+++ app/http/dtos/verify_answer_req.go
@@ -3,5 +3,5 @@ package dtos
 type VerifyAnswerRequest struct {
 	Answer   string `json:"answer" binding:"required"`
 	Problem  string `json:"problem" binding:"required"`
-	Language string `json:"language" binding:"required"`
+	Language string `json:"language"`
 }
