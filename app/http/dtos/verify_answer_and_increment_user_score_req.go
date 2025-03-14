5d25ef484f3b345255576783ec10ab1d0a0b06ee
vladisluw <komkov222111@gmail.com>
2025-03-14 15:59:22 +0500

    Update verify_answer_and_increment_user_score_req.go

--- app/http/dtos/verify_answer_and_increment_user_score_req.go
+++ app/http/dtos/verify_answer_and_increment_user_score_req.go
@@ -2,6 +2,6 @@ package dtos
 
 type VerifyAnswerAndIncrementUserScoreRequest struct {
 	ProblemId uint   `json:"problem_id" binding:"required"`
-	Language  string `json:"language" binding:"required"`
+	Language  string `json:"language"`
 	Answer    string `json:"answer" binding:"required"`
 }
