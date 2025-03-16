9cbd5aaa6a007376275a239a7c9ba34cb23821c3
vladisluw <komkov222111@gmail.com>
2025-03-16 07:33:31 +0500

    Update api.ts

--- client/src/core/api/api.ts
+++ client/src/core/api/api.ts
@@ -1,6 +1,6 @@
+export const HOST_URL = "https://web:80"
 // export const HOST_URL = "https://educgpt.ru"
-// export const HOST_URL = "https://educgpt.ru"
-export const HOST_URL = "http://localhost:8080"
+// export const HOST_URL = "http://localhost:8080"
 
 export const HOST_URL_PROD = "https://educgpt.ru"
 
