1d8ae28454cfe7f70b21d8b097bb951e00ed5d4b
vladisluw <komkov222111@gmail.com>
2025-03-22 19:58:36 +0500

    Update file_test.go

--- app/test/unit/file_test.go
+++ app/test/unit/file_test.go
@@ -1,1 +1,14 @@
 package unit
+
+import (
+	"educ-gpt/config/dic"
+	"educ-gpt/services"
+)
+
+var (
+	fileSrv services.FileService
+)
+
+func TestInitFileService() {
+	fileSrv = dic.FileService()
+}
